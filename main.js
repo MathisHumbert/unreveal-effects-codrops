3a0cb3a8d69eb0ce7cfab67240766c9435f6a56e
MathisHumbert <mathishumbert71@gmail.com>
2022-11-23 21:35:53 +0100

    working on preview

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.css';
 import gsap from 'gsap';
 
-const initContentHover = () => {
+const init = () => {
   const contentImgWrapper = document.querySelectorAll(
     '.content__item__img__wrapper'
   );
@@ -9,6 +9,43 @@ const initContentHover = () => {
     '.content__item__title .oh__inner'
   );
   const contentImg = document.querySelectorAll('.content__item__img');
+  const contentCaption = document.querySelectorAll('.content__item__caption');
+  const contentOverlay = document.querySelector('.content__overlay');
+  const previewItem = document.querySelectorAll('.preview__item');
+  const previewBack = document.querySelector('.preview__back');
+
+  const onMouseClick = (index) => {
+    const tl = gsap.timeline();
+
+    previewItem[index].classList.add('preview__item--current');
+
+    tl.set(contentOverlay, { transformOrigin: '0% 0%', scaleX: 0.25 })
+      .to(
+        contentTitle[index],
+        {
+          duration: 0.2,
+          ease: 'power1.in',
+          yPercent: 100,
+        },
+        0
+      )
+      .to(
+        contentCaption[index],
+        {
+          duration: 0.2,
+          ease: 'power1.in',
+          yPercent: 10,
+        },
+        0
+      )
+      .to(contentOverlay, {
+        scaleY: 1,
+      })
+      .to(contentOverlay, {
+        scaleX: 1,
+      })
+      .to(previewBack, { opacity: 1 });
+  };
 
   const onMouseEnter = (index) => {
     const tl = gsap.timeline({ defaults: { duration: 0.8, ease: 'power4' } });
@@ -50,14 +87,12 @@ const initContentHover = () => {
   };
 
   contentImgWrapper.forEach((content, index) => {
+    content.addEventListener('click', () => onMouseClick(index));
+
     content.addEventListener('mouseenter', () => onMouseEnter(index));
 
     content.addEventListener('mouseleave', () => onMouseLeave(index));
   });
-};
-
-const init = () => {
-  initContentHover();
 };
 
 window.addEventListener('load', () => init());
