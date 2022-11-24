62cb85488914979443df3c055103669679387566
MathisHumbert <mathishumbert71@gmail.com>
2022-11-24 19:02:56 +0100

    wip transition between content to preview

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 import './style.css';
 import gsap from 'gsap';
+import { Flip } from 'gsap/Flip';
+
+gsap.registerPlugin(Flip);
 
 const init = () => {
+  let inPreview = false;
+
+  let isAnimating = false;
+
+  const contentItem = document.querySelectorAll('.content__item');
   const contentImgWrapper = document.querySelectorAll(
     '.content__item__img__wrapper'
   );
@@ -13,45 +21,142 @@ const init = () => {
   const contentOverlay = document.querySelector('.content__overlay');
   const previewItem = document.querySelectorAll('.preview__item');
   const previewBack = document.querySelector('.preview__back');
+  const previewTitle = document.querySelectorAll(
+    '.preview__item__title .oh__inner'
+  );
+  const previewSubtitle = document.querySelectorAll(
+    '.preview__item__subtitle .oh__inner'
+  );
+  const previewMeta = document.querySelectorAll(
+    '.preview__item__meta .oh__inner'
+  );
+  const previewBoxLeftTitle = document.querySelectorAll(
+    '.preview__item__box--left .preview__item__box__title .oh__inner'
+  );
+  const previewBoxLeftDescription = document.querySelectorAll(
+    '.preview__item__box--left .preview__item__box__description'
+  );
+  const previewBoxRightTitle = document.querySelectorAll(
+    '.preview__item__box--right .preview__item__box__title .oh__inner'
+  );
+  const previewBoxRightDescription = document.querySelectorAll(
+    '.preview__item__box--right .preview__item__box__description'
+  );
+  const previewImgOuter = document.querySelectorAll(
+    '.preview__item__img__outer'
+  );
 
   const onMouseClick = (index) => {
-    const tl = gsap.timeline();
+    if (inPreview || isAnimating) return;
 
-    previewItem[index].classList.add('preview__item--current');
+    isAnimating = true;
 
-    tl.set(contentOverlay, { transformOrigin: '0% 0%', scaleX: 0.25 })
+    const { x: contentOverlayX } = contentItem[index].getBoundingClientRect();
+    const contentOverlayOrigin = index % 2 ? '0% 100%' : '0% 0%';
+
+    const tl = gsap.timeline({
+      defaults: {
+        duration: 0.8,
+        ease: 'power4.inOut',
+      },
+      onStart: () => {
+        inPreview = true;
+        gsap.set(contentItem[index], { zIndex: 10 });
+        gsap.set(contentOverlay, {
+          transformOrigin: contentOverlayOrigin,
+          scaleX: 0.25,
+          x: contentOverlayX,
+        });
+        gsap.set(
+          [
+            previewTitle[index],
+            previewSubtitle[index],
+            previewMeta[index],
+            previewBoxLeftTitle[index],
+            previewBoxRightTitle[index],
+          ],
+          {
+            yPercent: 100,
+          }
+        );
+        gsap.set(
+          [previewBoxLeftDescription[index], previewBoxRightDescription[index]],
+          { yPercent: 15, opacity: 0 }
+        );
+
+        previewItem[index].classList.add('preview__item--current');
+        document.body.classList.add('preview-open');
+      },
+      onComplete: () => (isAnimating = false),
+    });
+
+    tl.addLabel('start')
+      .addLabel('content', 'start+=0.6')
       .to(
         contentTitle[index],
         {
-          duration: 0.2,
-          ease: 'power1.in',
-          yPercent: 100,
+          yPercent: index % 2 ? -100 : 100,
         },
         0
       )
       .to(
         contentCaption[index],
         {
-          duration: 0.2,
-          ease: 'power1.in',
-          yPercent: 10,
+          yPercent: index % 2 ? -10 : 10,
+          opacity: 0,
         },
         0
       )
-      .to(contentOverlay, {
-        scaleY: 1,
-      })
-      .to(contentOverlay, {
-        scaleX: 1,
-      })
-      .to(previewBack, { opacity: 1 });
+      .to(
+        contentOverlay,
+        {
+          scaleY: 1,
+        },
+        0
+      )
+      .to(
+        contentOverlay,
+        {
+          scaleX: 1,
+          x: 0,
+        },
+        'content'
+      )
+      .add(() => {
+        const flip = Flip.getState(contentImgWrapper);
+        previewImgOuter[index].appendChild(contentImgWrapper[index]);
+        Flip.from(flip, {
+          duration: 0.8,
+          ease: 'power4.inOut',
+          absolute: true,
+        });
+      }, 'content')
+      .to(
+        [
+          previewTitle[index],
+          previewSubtitle[index],
+          previewMeta[index],
+          previewBoxLeftTitle[index],
+          previewBoxRightTitle[index],
+        ],
+        { duration: 1.1, ease: 'expo', yPercent: 0 },
+        'content+=0.3'
+      )
+      .to(
+        [previewBoxLeftDescription[index], previewBoxRightDescription[index]],
+        { duration: 1.1, ease: 'expo', yPercent: 0, opacity: 1 },
+        'content+=0.3'
+      )
+      .to(previewBack, { opacity: 1 }, 'content');
   };
 
   const onMouseEnter = (index) => {
+    if (inPreview) return;
+
     const tl = gsap.timeline({ defaults: { duration: 0.8, ease: 'power4' } });
 
     tl.addLabel('start', 0)
-      .set(contentTitle[index], { transformOrigin: '0 50%' }, 0)
+      .set(contentTitle[index], { transformOrigin: '0% 50%' }, 0)
       .to(
         contentTitle[index],
         {
@@ -79,11 +184,50 @@ const init = () => {
   };
 
   const onMouseLeave = (index) => {
+    if (inPreview) return;
+
     gsap.to([contentImgWrapper[index], contentImg[index]], {
       scale: 1,
       duration: 0.8,
       ease: 'power4',
     });
+  };
+
+  const onButtonClick = () => {
+    if (isAnimating) return;
+    isAnimating = true;
+
+    const previewInView = document.querySelector('.preview__item--current');
+    const index = +previewInView.getAttribute('data-id');
+
+    const { x: contentOverlayX } = contentItem[index].getBoundingClientRect();
+
+    const tl = gsap.timeline();
+
+    tl.to(contentOverlay, { scaleX: 0.25, x: contentOverlayX })
+      .to(
+        [
+          previewTitle[index],
+          previewSubtitle[index],
+          previewMeta[index],
+          previewBoxLeftTitle[index],
+          previewBoxRightTitle[index],
+        ],
+        {
+          yPercent: 100,
+        },
+        0
+      )
+      .to(
+        [previewBoxLeftDescription[index], previewBoxRightDescription[index]],
+        { yPercent: 15, opacity: 0 },
+        0
+      )
+      .to(previewBack, { opacity: 0 }, 0)
+      .to(contentOverlay, {
+        scaleY: 0,
+        onComplete: () => (previewInView.className = 'preview__item'),
+      });
   };
 
   contentImgWrapper.forEach((content, index) => {
@@ -93,6 +237,8 @@ const init = () => {
 
     content.addEventListener('mouseleave', () => onMouseLeave(index));
   });
+
+  previewBack.addEventListener('click', onButtonClick);
 };
 
 window.addEventListener('load', () => init());
